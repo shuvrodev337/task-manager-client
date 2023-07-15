aea1e29cca9029c12ab296e0647fe7583418145a
unknown <shuvrodevmondal337@gmail.com>
2023-07-16 01:16:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+    <button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Button</button>
+<button className="btn btn-secondary">Button</button>
+<button className="btn btn-accent">Button</button>
+<button className="btn btn-ghost">Button</button>
+<button className="btn btn-link">Button</button>
+    </>
+  )
+}
+
+export default App
